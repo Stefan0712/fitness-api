9aeab4502641b6e096aafbdb8902a8ee3dd5e6f2
Stefan0712 <s.vladulescu@gmail.com>
2025-05-13 13:16:29 +0300

    Added routes for updating the profile and for getting only the need profile info for the edit profile page

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -9,7 +9,6 @@ const router = express.Router();
 
 router.get('/private', authenticateUser, async (req, res) => {
     const userId = req.user.id; 
-    console.log(req.body)
     try {
         const user = await User.findById(userId);
         if (!user) {
@@ -27,6 +26,77 @@ router.get('/private', authenticateUser, async (req, res) => {
     
 });
 
+
+// Get profile info for the edit page
+router.get('/update-user', authenticateUser, async (req, res)=>{
+    const userId = req.user.id; 
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            console.log('User not found');
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const profileData = {
+            username: user.username,
+            name: user.name,
+            bio: user.bio,
+            age: user.age,
+            gender: user.gender,
+            height: user.height,
+            weight: user.weight,
+            profileSettings: user.profileSettings
+        }
+        res.status(200).json({
+            message: 'Profile data for updating user profile',
+            user: profileData
+        });
+    } catch (error) {
+        console.error('Error finding user:', error.message);
+        return res.status(500).json({ message: 'Server error' });
+    }
+})
+
+// Update user's profile
+router.put('/update-user', authenticateUser, async (req, res)=>{
+    try {
+        const {
+            username,
+            name,
+            bio,
+            age,
+            gender,
+            height,
+            weight,
+            profileSettings
+        } = req.body;
+        // Validate inputs
+        const updatedFields = {
+            ...(username && { username }),
+            ...(name && { name }),
+            ...(bio && { bio }),
+            ...(age && { age }),
+            ...(gender && { gender }),
+            ...(height && { height }),
+            ...(weight && { weight }),
+            ...(profileSettings && { profileSettings })
+        };
+        const user = await User.findByIdAndUpdate(req.user.id,
+            { $set: updatedFields },
+            { new: true } 
+        );
+        if (!user) {
+            console.log('User not found');
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json({
+            message: 'Successfully updated profile',
+        });
+    } catch (error) {
+        console.error('Error finding user:', error.message);
+        return res.status(500).json({ message: 'Server error' });
+    }
+})
 
 
 
